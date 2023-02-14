f2450e8afddd381c94c7f6fd5b30e06d88fd4171
Abeera Tahir <abeeratahir2018@gmail.com>
2023-02-15 02:46:32 +0500

    Add list all genres method

--- src/modules/music_module.rb
+++ src/modules/music_module.rb
@@ -1,29 +1,36 @@
 require_relative '../classes/music_album.rb'
 require_relative '../classes/genre.rb'
+require_relative './genre_module.rb'
 
 module MusicModule
+  include GenreModule
+
   def add_a_music_album
     print 'Is it on spotify? [y/n] : '
     on_spotify = gets.chomp.to_s.downcase == 'y'
     print 'Please enter the published date : '
     published_date = gets.chomp.to_s
+
     music_album = MusicAlbum.new(on_spotify, published_date)
     @music_albums << music_album
-    puts 'Album added successfully!'
+
+    genre = add_genre
+    genre.add_item(music_album)
+
+    puts "Album of #{genre.name} genre added successfully!"
   end
 
-  def list_music_albums
+  def list_all_music_albums
     if @music_albums.empty?
       puts 'No album added!'
     else
       @music_albums.each_with_index do |album, index|
-        print "#{index + 1}. "
+        print "#{index + 1}. Published date : #{album.publish_date}, "
         if album.on_spotify
-          print "Album available on spotify." 
+          puts "Available on spotify." 
         else
-          print "Album not available on spotify."
+          puts "Not available on spotify."
         end
-        puts " Published date : #{album.publish_date}"
       end
     end
   end
