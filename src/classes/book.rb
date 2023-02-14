9ba44f44f7860b5f7e4a002f309cb9734320437e
jellywiz <muzaffer.dler@gmail.com>
2023-02-14 17:58:41 +0300

    Update book class

--- src/classes/book.rb
+++ src/classes/book.rb
@@ -3,8 +3,8 @@ require './item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state)
-    super()
+  def initialize(publish_date, publisher, cover_state)
+    super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
