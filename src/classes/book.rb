932b58f67f148e68e95b362dad55548fe5e64d93
jellywiz <muzaffer.dler@gmail.com>
2023-02-14 17:39:38 +0300

    Update book class

--- src/classes/book.rb
+++ src/classes/book.rb
@@ -1,14 +1,15 @@
-require_relative '../items'
+require './item'
+
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(date, publisher, cover_state, archived: false, id: nil)
-    super(id, date, archived: archived)
+  def initialize(publisher, cover_state)
+    super()
     @publisher = publisher
     @cover_state = cover_state
   end
 
-  def can_be_archived?
+  def can_be_archived?()
     super || @cover_state == 'bad'
   end
 end
