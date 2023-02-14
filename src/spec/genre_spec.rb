9cd3cd344629db36a30d2037752a3f4e715bfb97
Abeera Tahir <abeeratahir2018@gmail.com>
2023-02-14 23:47:41 +0500

    Add tests for genre class methods

---
+++ src/spec/genre_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../classes/genre.rb'
+require_relative '../item.rb'
+
+describe Genre do
+  before :each do
+    @genre = Genre.new 'Abeera'
+  end
+
+  it 'takes one parameters and returns a Genre object' do
+    expect(@genre).to be_an_instance_of Genre
+  end
+
+  it "Genre items list should be empty" do
+    expect(@genre.items.length).to eql 0
+  end
+
+  it "Genre items list should have one item" do
+    @genre.add_item(Item.new('2023-02-01'))
+    expect(@genre.items.length).to eql 1
+  end
+end
