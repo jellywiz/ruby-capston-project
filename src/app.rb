cb4831fb63315863c71f29a783706deb5d0ae28c
Abeera Tahir <abeeratahir2018@gmail.com>
2023-02-15 20:13:25 +0500

    Preserve music albums and genre data

--- src/app.rb
+++ src/app.rb
@@ -1,12 +1,13 @@
 require_relative './modules/music_module'
 require_relative './modules/genre_module'
+require_relative './storage.rb'
 
 class App
   include MusicModule
   include GenreModule
   def initialize
-    @music_albums = []
-    @genres = []
+    @music_albums = Storage.load_data('music_albums')
+    @genres = Storage.load_data('genres')
   end
   ACTIONS = {
     1 => :list_books,
@@ -47,7 +48,12 @@ class App
       show_interactive_console
       option = gets.chomp.to_i
       option = ACTIONS[option]
-      break if option == :exit
+      if option == :exit
+        puts "Thank you for using the app!"
+        Storage.save_data('music_albums', @music_albums) unless @music_albums.empty?
+        Storage.save_data('genres', @genres) unless @genres.empty?
+        break
+      end
 
       if option
         send(option)
