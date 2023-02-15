b6803970121e7d50aff37fede3921471657c1ced
dialite <raymondolisa01@gmail.com>
2023-02-15 16:22:39 +0100

    Create add a game method in game module

--- src/modules/game_module.rb
+++ src/modules/game_module.rb
@@ -1,9 +1,24 @@
 require_relative '../classes/game'
-
-def list_all_games
-  puts 'Music Albums: '
-  @games.each_with_index do |game, index|
-    puts "#{index + 1}) Multiplayer: #{game.multiplayer}, Publish Date: #{games.publish_date},
-    Last played date: #{game.last_played_at}"
+module GameModule
+  def list_all_games
+    puts 'Music Albums: '
+    @games.each_with_index do |game, index|
+      puts "#{index + 1}) Multiplayer: #{game.multiplayer}, Publish Date: #{games.publish_date},
+      Last played date: #{game.last_played_at}"
+    end
+  end
+  
+  def add_a_game
+    puts 'Please write multiplayer: '
+      multiplayer = gets.chomp
+  
+      puts 'Please write date of publish [Enter date in format (yyyy-mm-dd)]'
+      publish_date = gets.chomp
+  
+      puts 'Please write last played date [Enter date in format (yyyy-mm-dd)]'
+      last_played_date = gets.chomp
+  
+      @games.push(Game.new(publish_date, multiplayer, last_played_date))
+      puts 'Game is created'
   end
 end
