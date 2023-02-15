9209ad9f5034be3eed0a781db51f194b8c3a9507
jellywiz <muzaffer.dler@gmail.com>
2023-02-15 21:13:55 +0300

    Fix item Class

--- src/item.rb
+++ src/item.rb
@@ -2,6 +2,7 @@ require 'date'
 
 class Item
   attr_accessor :genre, :author, :source, :label, :publish_date, :archived
+  attr_reader :id
 
   def initialize(publish_date, archived: false)
     @id = Random.rand(1..1000)
@@ -15,7 +16,25 @@ class Item
 
   def move_to_archive
     return unless can_be_archived?
-
     @archived = true
   end
+
+  def add_source(source)
+    @source = source
+  end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.add_item(self) unless genre.items.include?(self)
+  end
+  def add_author(author)
+    @author = author
+  end
+
+  def add_label(label)
+    @label = label
+    label.add_item(self) unless label.items.include?(self)
+  end
+
+
 end
