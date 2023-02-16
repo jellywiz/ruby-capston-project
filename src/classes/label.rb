8ca554a19bd123c43f669abae2c5dcb811585a86
jellywiz <muzaffer.dler@gmail.com>
2023-02-16 11:44:27 +0300

    Fix Random id generator

--- src/classes/label.rb
+++ src/classes/label.rb
@@ -2,7 +2,7 @@ class Label
   attr_accessor :id, :title, :color, :items
 
   def initialize(title, color)
-    @id = rand(1000...1)
+    @id = Random.rand(1..1000)
     @title = title
     @color = color
     @items = []
