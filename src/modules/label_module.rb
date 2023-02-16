89d7e9d27983c585c9f80c272df1c25cbc5581ce
jellywiz <muzaffer.dler@gmail.com>
2023-02-16 11:44:59 +0300

    include index in list labels

--- src/modules/label_module.rb
+++ src/modules/label_module.rb
@@ -6,8 +6,9 @@ module LabelModule
     if @labels.empty?
       puts 'There are no labels in the catalog'
     else
+      # use index
       @labels.each_with_index do |label, index|
-        puts "[Label #{index + 1}]. Title: #{label.title}, Color: #{label.color}"
+        puts "No: #{index} ID #{label.id} Title: #{label.title}, Color: #{label.color}"
       end
     end
   end
